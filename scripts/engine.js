8e591ec8b698b7a62d7267192aa2dd9c2bbd1ff3
Pariador <pariador.corterian@gmail.com>
2015-07-27 22:37:25 +0300

    Add KineticJS

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,3 +1,22 @@
 (function engine() {
+	var stage = new Kinetic.Stage({
+			container: 'kinetic-container',
+			width: 1024,
+			height: 768
+		}),
+		gameLayer = new Kinetic.Layer(),
+		rect = new Kinetic.Rect({
+			x: 100,
+			y: 150,
+			width: 75,
+			height: 225,
+			fill: 'white',
+			stroke: 'darkgoldenrod',
+			lineWidth: 5
+		});
 
+	gameLayer.add(rect);
+	stage.add(gameLayer);
+
+	gameLayer.draw();
 })();
