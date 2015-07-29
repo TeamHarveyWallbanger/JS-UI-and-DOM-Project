cae8e8ed3747795b67137fd591f23db176a7ac68
boyanatsvetkova <boyana.tsvetkova@gmail.com>
2015-07-29 22:55:33 +0300

    obstacle logic added

--- scripts/Obstacle.js
+++ scripts/Obstacle.js
@@ -1,3 +1,38 @@
+var Obstacle = (function () {
+
+    var ANIMATION_FRAMERATE = 65;
+
+    function Obstacle(position, layer, image) {
+        this.position = position;
+        this._animation = new Animation(layer, image, 1, 1, position.x, position.y);
+        this._animation.start(ANIMATION_FRAMERATE);
+    }
+
+    Object.prototype.updateX = function(update) {
+        this.position.x += update;
+        this._animation.x = this.position.x;
+    };
+
+    Object.defineProperties(Obstacle.prototype, {
+        position: {
+            get: function () {
+                return this._position;
+            },
+
+            set: function (value) {
+                if (!value instanceof Position) {
+                    throw new Error('Value must be an instance of Position!');
+                }
+
+                this._position = value;
+            }
+        }
+    });
+
+    return Obstacle;
+})();
+
+
 // obstacle: {
 // 	collider: path,
 // 	image: image,
