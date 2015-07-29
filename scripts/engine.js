aa5ca1109b2aefe24a80247822c8d5e2ab66ca87
Kiril Borisov <borisov.kiril760@gmail.com>
2015-07-30 00:27:35 +0300

    engine.js without tests

--- scripts/engine.js
+++ scripts/engine.js
@@ -11,6 +11,7 @@ function engine() {
         }),
         gameLayer = new Kinetic.Layer();
 
+    stage.add(gameLayer);
 
     //var heroAnim = new Animation(gameLayer, Images['hero.png'], 3, 6, 0, 0, 5);
 //    heroAnim.start(100);
@@ -22,7 +23,7 @@ function engine() {
     var bg = new Kinetic.Rect({
         x: 0,
         y: 0,
-       width: 1000,
+        width: 1000,
         height: 750,
         draggable: false
     });
@@ -31,29 +32,22 @@ function engine() {
 
     backgroundLayer = new Kinetic.Layer();
     backgroundLayer.add(bg);
-
     stage.add(backgroundLayer);
-    stage.add(gameLayer);
-
+//======================================================================
+    var coinStartPos = new Position(900, 200);
     var radius = (Images['coin.png'].width / 10) / 2;
-    var coinStartPos = new Position(0 + radius, 0 + radius);
     var coin = new Coin(coinStartPos, radius, gameLayer, Images['coin.png']);
 
-    var obstacle = new Obstacle(obstacleStartPos, gameLayer, Images['BunchOfRocks.png']);
-    var obstacleStartPos = new Position(900, 300);
-
     var offset = 0;
+
     function gameAnimation() {
         requestAnimationFrame(gameAnimation);
-
+        coin.updateX(-5);
         offset = (offset - 7) % 1000;
         bg.fillPatternOffsetX(offset);
-
-        // coin.updateX(-5);
-        obstacle.updateX(-5);
-
         backgroundLayer.draw();
         gameLayer.draw();
+
     }
 
     gameAnimation();
