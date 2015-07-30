da9113e33230224e6a26608191eb601ee8edd22f
PavelDochev <pointofvieww@gmail.com>
2015-07-30 17:03:24 +0300

    timer
    simple timer added

--- scripts/engine.js
+++ scripts/engine.js
@@ -3,7 +3,29 @@ function engine() {
         setTimeout(engine, 50);
         return;
     }
-
+    function countdown(minutes) {
+        var seconds = 60;
+        var mins = minutes
+        function tick() {
+            var counter = document.getElementById("timer");
+            var current_minutes = mins-1
+            seconds--;
+            counter.innerHTML = 
+            current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+            if( seconds > 0 ) {
+                  setTimeout(tick, 1000);
+            }  
+            else {
+            if(mins > 1){
+                 
+               // countdown(mins-1);  
+               setTimeout(function () { countdown(mins - 1); }, 1000);
+                     
+                }
+            }
+        }
+        tick();
+    }
     var bgLayer,
         gameLayer,
         background,
@@ -42,7 +64,7 @@ function engine() {
         bgLayer.draw();
         gameLayer.draw();
     }
-
+    countdown(2);
     gameAnimation();
 }
 engine();
