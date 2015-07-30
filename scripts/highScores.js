efccadccbeb251cb7b3fa018705f0354d726a240
mdraganov <mariyandraganov@gmail.com>
2015-07-30 21:36:47 +0300

    updated loadScores

--- scripts/highScores.js
+++ scripts/highScores.js
@@ -1,6 +1,7 @@
 var highScores = (function () {
     var highScores,
-		tenHighest = [];
+		sortedHighScores = [],
+        NUMBER_OF_HIGH_SCORES_TO_SAVE = 10;
 
     highScores = {
         loadHighScores: loadScores,
@@ -15,22 +16,22 @@ var highScores = (function () {
             playerScore,
             currentItem;
 
-        for (i = 0; i < 10; i += 1) {
+        for (i = 0; i < NUMBER_OF_HIGH_SCORES_TO_SAVE; i += 1) {
             if (!localStorage.getItem('score' + (i + 1))) {
-                //tenHighest[i] = '--- : ---';
-                continue;
+                break;
             }
 
             currentItem = localStorage.getItem('score' + (i + 1));
 
-            tenHighest[i] = currentItem;
+            sortedHighScores[i][0] = currentItem.match(/[\w\s]+/)[0];
+            sortedHighScores[i][1] = +currentItem.match(/[\w\s]+/)[0];
         }
     }
 
     function getScores() {
-        return tenHighest.slice();
+        return sortedHighScores.slice();
     }
-
+    
     function resetScores() {
         var i,
             len = localStorage.length;
@@ -38,36 +39,36 @@ var highScores = (function () {
         for (i = 0; i < len; i += 1) {
             localStorage.removeItem('score' + (i + 1));
         }
-
-        loadScores();
     }
 
     //Checks if player's score is among the ten highest and adds it if so
     function addCurrentPlayerScore(name, score) {
         var i,
-            len,
             playerName,
             playerScore,
-            lowestScore = tenHighest[tenHighest.length - 1][1];
+            length = sortedHighScores.length,
+            lowestScore = sortedHighScores[sortedHighScores.length - 1][1];
 
-        if (tenHighest.length < 10) {
-            tenHighest.push([name, score]);
-            tenHighest.sort(function (a, b) {
-                return a[1] - b[1];
-            })
+        if (length < NUMBER_OF_HIGH_SCORES_TO_SAVE) {
+            sortedHighScores.push([name, score]);
+            if (length > 1) {
+                sortedHighScores.sort(function (a, b) {
+                    return a[1] - b[1];
+                })
+            }
         } else if (score > lowestScore) {
-            tenHighest[tenHighest.length - 1][0] = name;
-            tenHighest[tenHighest.length - 1][1] = score;
-            tenHighest.sort(function (a, b) {
+            sortedHighScores[length - 1][0] = name;
+            sortedHighScores[length - 1][1] = score;
+            sortedHighScores.sort(function (a, b) {
                 return a[1] - b[1];
             })
         }
 
         resetScores();
 
-        for (i = 0, len = tenHighest.length; i < len; i += 1) {
-            playerName = tenHighest[i][0];
-            playerScore = tenHighest[i][1];
+        for (i = 0, length = sortedHighScores.length; i < length; i += 1) {
+            playerName = sortedHighScores[i][0];
+            playerScore = sortedHighScores[i][1];
 
             localStorage.setItem('score' + (i + 1), playerName + ':' + playerScore)
         }
